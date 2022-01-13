e1607c87e7deec95925552ee16b54af5b3b64fe8
Alexey <kret906alex@gmail.com>
2022-01-13 17:50:43 +0300

    Initial commit

---
+++ backend/src/main/java/com/SpringBootStarter.java
@@ -0,0 +1,15 @@
+package com;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Meeting application backend API", version = "1.0"))
+public class SpringBootStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStarter.class);
+    }
+}
